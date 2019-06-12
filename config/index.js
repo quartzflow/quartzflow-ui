1a5d5884ab8258c83f8bf73a4f594b24097b5d1b
travis-dr <8000719+travis-dr@users.noreply.github.com>
2019-06-12 10:13:04 +1000

    Rebrand JobScheduler to QuartzFlow

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/api': {
-        target: 'http://localhost:5000/scheduler/',
+        target: 'http://localhost:5000/quartzflow/',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
